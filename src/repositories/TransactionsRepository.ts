3707409ab81aa17113605a02ff25d1dd1a5b2e3c
Fábio <fabiomirandafig@gmail.com>
2021-06-14 22:37:34 -0300

    Versão final

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,12 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
 import Transaction from '../models/Transaction';
+import Balance from '../models/Balance';
 
-interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +17,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const inc = this.transactions.reduce((Total, elemento) => {
+      if (elemento.type === 'income') {
+        return (Total += elemento.value);
+      }
+      return Total;
+    }, 0);
+
+    const out = this.transactions.reduce((Total, elemento) => {
+      if (elemento.type === 'outcome') {
+        return (Total += elemento.value);
+      }
+      return Total;
+    }, 0);
+
+    const b = inc - out;
+
+    const balance = new Balance({
+      total: b,
+      income: inc,
+      outcome: out,
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
